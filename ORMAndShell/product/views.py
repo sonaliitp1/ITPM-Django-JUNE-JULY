560ac9ba733677d4030b7252371a4aacb944a58e
sonaliitp1 <SonaliS@itpreneurpune.com>
2026-01-07 11:27:16 +0530

    ORM and SHELL

--- ORMAndShell/product/views.py
+++ ORMAndShell/product/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import ProductInfo
 
 # Create your views here.
+
+def home(request):
+    return render(request,'home.html')
+
+def showproduct(request):
+    data = ProductInfo.objects.all()
+
+    context ={'d':data}
+
+    return render(request,'showproducts.html',context) 
