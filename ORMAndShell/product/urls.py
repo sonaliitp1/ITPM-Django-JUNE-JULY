5d462bbc486e482f1e21bf4c2279ce860eac2d85
sonaliitp1 <SonaliS@itpreneurpune.com>
2026-01-12 11:20:32 +0530

    PUT and Delete

--- ORMAndShell/product/urls.py
+++ ORMAndShell/product/urls.py
@@ -5,5 +5,7 @@ from .import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('s/',views.showproduct,name='show'),
-    path('a/',views.addproduct,name='add')
+    path('a/',views.addproduct,name='add'),
+    path('edit/<int:id>',views.edit,name='edit'),
+    path('delete/<int:id>',views.delete,name='delete')
 ]
