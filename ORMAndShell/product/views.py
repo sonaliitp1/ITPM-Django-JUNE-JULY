f316538a65ab27423a64fea196eb46b3399aa26a
sonaliitp1 <sonali.s@itpreneurpune.com>
2026-01-13 10:03:08 +0530

    Add files via upload

--- ORMAndShell/product/views.py
+++ ORMAndShell/product/views.py
@@ -24,3 +24,25 @@ def addproduct(request):
         return redirect("show")
     else:          
         return render(request,"addproduct.html")
+    
+def edit(request,id):
+    obj =  ProductInfo.objects.get(pid=id)
+    context={'obj':obj}
+    if request.method == "POST":
+        pid = request.POST.get("pid")
+        pname = request.POST.get("pname")
+        category = request.POST.get("category")
+        stock = request.POST.get("stock")
+        obj.pid = pid
+        obj.pname = pname 
+        obj.category =category
+        obj.stock = stock
+        obj.save()
+        return redirect("show")
+    else:
+        return render(request,'edit.html',context)
+    
+def delete(request,id):
+    obj = ProductInfo.objects.get(pid=id)
+    obj.delete()
+    return redirect("show")
