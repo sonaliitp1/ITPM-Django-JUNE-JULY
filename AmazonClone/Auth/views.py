2446dafd59983eca65ecbd49054c95a2c6da88ea
sonaliitp1 <sonali.s@itpreneurpune.com>
2026-01-22 11:16:19 +0530

    Register User

--- AmazonClone/Auth/views.py
+++ AmazonClone/Auth/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
 
-# Create your views here.
+# Create your views her
 def home(request):
     return render(request,'home.html')
 
@@ -9,7 +10,22 @@ def loginu(request):
     return render(request,'loginu.html')
 
 def registeru(request):
-    pass 
+    if request.method =="POST":
+        un = request.POST.get("username")
+        em = request.POST.get("email")
+        password = request.POST.get("password")
+        cpassword = request.POST.get("cpassword")
+        obj = User.objects.filter(username=un).exists()
+        if obj:
+            return render(request,'registeru.html',{'error':"User Is Already Exists....Please try again"})
+        elif password!=cpassword:
+             return render(request,'registeru.html',{'error':"Password and ConfirmPassword do not match"})
+        else:
+            obj = User(username=un,email=em,password=password)
+            obj.save()
+            return redirect("loginu")
+    else:
+        return render(request,'registeru.html')
 
 def logoutu(request):
     pass
