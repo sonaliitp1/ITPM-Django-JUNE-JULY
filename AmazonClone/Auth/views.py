8fe558aba48ec36a85d7876ab3a3f390ea6f77bc
sonaliitp1 <sonali.s@itpreneurpune.com>
2026-01-23 11:10:34 +0530

    Login and Logout

--- AmazonClone/Auth/views.py
+++ AmazonClone/Auth/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views her
 def home(request):
     return render(request,'home.html')
 
-
 def loginu(request):
-    return render(request,'loginu.html')
+    if request.method == "POST":
+        un = request.POST.get("username")
+        ps = request.POST.get("password")
+        try:
+            user =  User.objects.get(username=un)
+        except User.DoesNotExist:
+            return render(request,"loginu.html",{"error":"User Not Found ... Please Try Again"})
+        user = authenticate(request,username=un,password=ps)
+        if user is not None:
+            login(request,user)
+            return redirect("home")
+        else:
+            return render(request,"loginu.html",{"error":"Invalid Credentials..."})     
+        
+    else:
+        return render(request,'loginu.html')
 
 def registeru(request):
     if request.method =="POST":
@@ -21,11 +36,13 @@ def registeru(request):
         elif password!=cpassword:
              return render(request,'registeru.html',{'error':"Password and ConfirmPassword do not match"})
         else:
-            obj = User(username=un,email=em,password=password)
+            obj = User.objects.create_user(username=un,email=em,password=password)
             obj.save()
             return redirect("loginu")
+        
     else:
         return render(request,'registeru.html')
 
 def logoutu(request):
-    pass
+    logout(request)
+    return redirect("loginu")
